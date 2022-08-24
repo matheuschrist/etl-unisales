122da6e2476a4f8fbb98d9b5b730e012ac62beb4
matheuschrist <matheusbrau12@gmail.com>
2022-08-24 17:08:11 -0300

    Enviando ajustes

--- extration.py
+++ extration.py
@@ -8,7 +8,6 @@ import pandas as pd
 def extracao_dados():
         
     olist_customers = 'base_dados/olist_customers_dataset.csv' 
-    olist_geolocation_dataset='base_dados/olist_geolocation_dataset.csv'
     olist_order_items_dataset='base_dados/olist_order_items_dataset.csv'
     olist_order_payments_dataset='base_dados/olist_order_payments_dataset.csv'
     olist_orders_dataset='base_dados/olist_orders_dataset.csv'
@@ -17,14 +16,54 @@ def extracao_dados():
 
 
     customers = pd.read_csv(olist_customers)
-    geolocation=pd.read_csv(olist_geolocation_dataset)
     order_items=pd.read_csv(olist_order_items_dataset)
     payments=pd.read_csv(olist_order_payments_dataset)
     orders=pd.read_csv(olist_orders_dataset)
     sallers=pd.read_csv(olist_sellers_dataset)
+    
+    
+    
+    
    
     
-    return customers,geolocation,order_items,payments,orders,sallers
+    return customers,order_items,payments,orders,sallers
+
+
+
+
+def create_date_table(start='1980-01-01', end='2021-12-31'):
+    start_ts = pd.to_datetime(start).date()
+
+    end_ts = pd.to_datetime(end).date()
+
+  
+    dates =  pd.DataFrame(index=pd.date_range(start_ts, end_ts))
+    dates.index.name = 'Date'
+
+    days_names = {
+        i:name
+        for i, name in enumerate(['Domingo', 'Segunda-feria', 'Terça-feira',
+                      'Quarta-Feira', 'Quinta-feria', 'Sexta-feira', 
+                      'Sabado'])
+    }
+    
+    dates['DiaSemana'] = dates.index.dayofweek.map(days_names.get)
+    dates['Dia'] = dates.index.day
+    dates['Semana'] = dates.index.week
+    dates['Mês'] = dates.index.month
+    dates['Trimestre'] = dates.index.quarter
+    dates['Semestre'] = dates.index.month.map(lambda mth: 1 if mth <7 else 2)
+    dates['Ano'] = dates.index.year
+    dates['date_id'] = dates['Dia'].astype(str)+dates['Mês'].astype(str)+dates['Ano'].astype(str)
+    dates.reset_index(inplace=True)
+    
+    
+      
+
+    return dates
+
+
+
 
 
 
