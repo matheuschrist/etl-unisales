8dac6888c17f37da9a03ec7d0a15c1299db6531a
matheuschrist <matheusbrau12@gmail.com>
2022-08-24 17:07:39 -0300

    Organizando código

--- criando_dw.py
+++ criando_dw.py
@@ -1,18 +1,19 @@
 
-
-from transformacao import transformation as trans
-
 from sqlalchemy import create_engine
 
-#Carregamento no DW"""
-fato_orders,sallers,customers,geolocation=trans()
+def conection_dw():
+    #Carregamento no DW"""
+    engine = create_engine(
+        'postgresql://dbaadmin:{senha}@dw-unisales-dados.postgres.database.azure.com:5432/postgres')
+    conn=engine.connect()
+    print("Connection established")
 
 
-engine = create_engine(
-    'postgresql://dbaadmin:{senha}@dw-unisales-dados.postgres.database.azure.com:5432/postgres')
-conn=engine.connect()
-print("Connection established")
+    return conn
 
+
+
+"""""
 #insere os dados da DataFrame no banco de dados 
 fato_orders.to_sql(name='fato_orders' , con=conn , schema = None , if_exists = 'replace' , index = True , index_label = None ,
                    chunksize = None , dtype = None , method = None )
@@ -26,3 +27,6 @@ print('Executado')
 geolocation.to_sql(name='geolocation' , con=conn , schema = None , if_exists = 'replace' , index = True , index_label = None ,
                    chunksize = None , dtype = None , method = None )
 print('Executado')
+
+
+"""
--- transformacao.py
+++ transformacao.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from extration import extracao_dados as ex
+import math
 
 
 
@@ -8,7 +9,7 @@ def transformation():
     """#Transoformação dos dados """
 
 
-    customers,geolocation,order_items,payments,orders,sallers=ex()
+    customers,order_items,payments,orders,sallers=ex()
     #Excluindo as colunas da tabela de pedidos  que não serão utilizadas 
     orders.drop(labels=['order_purchase_timestamp','order_delivered_carrier_date','order_delivered_customer_date','order_estimated_delivery_date'], axis=1,inplace=True)
 
@@ -19,22 +20,55 @@ def transformation():
     order_items.drop(labels=['shipping_limit_date'], axis=1,inplace=True)
 
     #Excluindo as colunas da tabela de clientes  que não serão utilizadas 
-    customers.drop(['customer_unique_id','customer_city','customer_state'], axis=1,inplace=True)
+    customers.drop(['customer_unique_id'], axis=1,inplace=True)
 
-    #Excluindo as colunas da tabela de vendedores que não serão utilizadas 
-
-    sallers.drop(['seller_city','seller_state'], axis=1,inplace=True)
+    #Excluindo as colunas da tabela de vendedores que não serão utilizadas
+    #sallers.drop(['seller_city','seller_state'], axis=1,inplace=True)
 
     #Excluindo as colunas da tabela de Localização que não serão utilizadas 
-    geolocation.drop(['geolocation_lat','geolocation_lng'], axis=1,inplace=True)
+    #geolocation.drop(['geolocation_lat','geolocation_lng'], axis=1,inplace=True)
 
 
+
+        
+    """#Transoformação a data para o mesmo padrão da tabela calendário"""
+
+    
+    orders['Dia'] = pd.DatetimeIndex(orders['order_approved_at']).day
+    orders['Mês'] = pd.DatetimeIndex(orders['order_approved_at']).month
+    orders['Ano'] = pd.DatetimeIndex(orders['order_approved_at']).year
+
+    #convertendo as datas para int
+    for mes in orders['Mês']:
+    #Verifica se possui valores vazios e adiciona zero caso for Verdadeiro
+        if math.isnan(mes) == True:
+            orders['Mês']=0
+        else:
+            orders['Mês']=int(mes)
+    for dia in orders['Dia']:
+        if math.isnan(dia) == True:
+            orders['Dia']=0
+        else:
+            orders['Dia']=int(dia)
+    for ano in orders['Ano']:
+        if math.isnan(ano) == True:
+            orders['Ano']=0
+        else:
+            orders['Ano']=int(ano)
+
+
+
+
+    orders['order_approved_at']= orders['Dia'].astype(str)+orders['Mês'].astype(str)+orders['Ano'].astype(str)
 
     #Criando a tabela Fato 
-    fato_orders = pd.merge(payments, orders, how = 'inner', on = 'order_id')
-    fato_orders= pd.merge(order_items, fato_orders, how = 'inner', on = 'order_id')
+    #Verificar o melhor parametro
+    orders=orders.rename(columns={'order_approved_at': 'data_id'})
+    fato_orders = pd.merge(orders,payments, how = 'left',on='order_id')
+    fato_orders= pd.merge(fato_orders,order_items, how = 'left', on = 'order_id')
 
 
-    return fato_orders,sallers,customers,geolocation
+
+    return fato_orders,sallers,customers
 
 
